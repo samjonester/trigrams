089c20a85c8ef8bbe3fef04f17822d18fa8d7197
Sam Jones <sam@samljones.com>
2016-09-26 12:08:00 -0400

    Creates second level type signature

--- haskell/src/Trigrams.hs
+++ haskell/src/Trigrams.hs
@@ -3,7 +3,18 @@ module Trigrams (trigramerate) where
 import Data.Map (Map)
 
 type Trigrams = Map String [String]
+type Trigram = [String]
+type Chunks = [[String]]
 
 
 trigramerate :: String -> Trigrams
-trigramerate = undefined
+trigramerate = joinTrigrams . createTrigrams . consByThree
+
+joinTrigrams :: [Trigram] -> Trigrams
+joinTrigrams = undefined
+
+createTrigrams :: Chunks -> [Trigram]
+createTrigrams = undefined
+
+consByThree :: String -> Chunks
+consByThree = undefined
