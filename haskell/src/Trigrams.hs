aa44762cbd5090815f15593a20aab9c2bbd9f50f
Sam Jones <sam@samljones.com>
2016-09-26 15:50:33 -0400

    Creates trigrams from chunks

--- haskell/src/Trigrams.hs
+++ haskell/src/Trigrams.hs
@@ -1,4 +1,4 @@
-module Trigrams (trigramerate, consByThree, consBy, lengthIs) where
+module Trigrams (trigramerate, consByThree, createTrigrams) where
 
 import Data.Map (Map)
 import Utils.Array (lengthIs,  consBy)
@@ -15,7 +15,7 @@ joinTrigrams :: [Trigram] -> Trigrams
 joinTrigrams = undefined
 
 createTrigrams :: Chunks -> [Trigram]
-createTrigrams = fmap (\(x:y:z) -> [x ++ y])
+createTrigrams = fmap (\(x:y:rest) -> unwords [x, y] : rest)
 
 consByThree :: String -> Chunks
 consByThree = takeWhile (lengthIs 3) . consBy 3 . words
--- haskell/test/TrigramsSpec.hs
+++ haskell/test/TrigramsSpec.hs
@@ -3,7 +3,7 @@ module TrigramsSpec (spec) where
 import Test.Hspec
 import Data.Map (Map, fromList)
 
-import Trigrams (trigramerate, consByThree)
+import Trigrams (trigramerate, consByThree, createTrigrams)
 
 spec :: Spec
 spec = do
@@ -15,3 +15,7 @@ spec = do
   describe "consByThree" $ do
     it "Should create arrays of 3 consecutive words in string" $ do
       consByThree "Row Row Row Your Boat" `shouldBe` [["Row", "Row", "Row"], ["Row", "Row", "Your"], ["Row", "Your", "Boat"]]
+
+  describe "createTrigrams" $ do
+    it "Should make a trigram from an array of three strings" $ do
+      createTrigrams [["a", "b", "c"], ["Foo", "Bar", "Baz"]] `shouldBe` [["a b", "c"], ["Foo Bar", "Baz"]]
